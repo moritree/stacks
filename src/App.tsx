484782acaca81e62687d9979e7660ef5e0689a00
code <leone.zuni@gmail.com>
2025-01-23 15:45:25 +1300

    Init tauri project

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import { useState } from "preact/hooks";
+import preactLogo from "./assets/preact.svg";
+import { invoke } from "@tauri-apps/api/core";
+import "./App.css";
+
+function App() {
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+    setGreetMsg(await invoke("greet", { name }));
+  }
+
+  return (
+    <main class="container">
+      <h1>Welcome to Tauri + Preact</h1>
+
+      <form
+        class="row"
+        onSubmit={(e) => {
+          e.preventDefault();
+          greet();
+        }}
+      >
+        <input
+          id="greet-input"
+          onInput={(e) => setName(e.currentTarget.value)}
+          placeholder="Enter a name..."
+        />
+        <button type="submit">Greet</button>
+      </form>
+      <p>{greetMsg}</p>
+    </main>
+  );
+}
+
+export default App;
