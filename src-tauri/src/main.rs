f9b213afa3c62b4b5bc9f19242dd4d519d7b936a
code <leone.zuni@gmail.com>
2025-01-27 21:12:52 +1300

    Merge lib.rs into main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,32 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod lua_init;
+use lua_init::{init_lua_thread, tick, update_entity_property};
+use tauri::Manager;
+
 fn main() {
-    stacks_lib::run()
+    tauri::Builder::default()
+        .setup(|app| {
+            let window = app.get_webview_window("main").unwrap();
+            let state = init_lua_thread(window.clone());
+
+            let state_clone = state.clone();
+            // Shutting down the clone WILL shut down the actual original state.
+            // The `mpsc::Sender` type specifically is designed to work this way
+            // a clone isn't a deep copy of the channel, but another handle to it
+            // We need a clone bc rust ownership means scope changes lose the var
+            window.on_window_event(move |event| {
+                if let tauri::WindowEvent::Destroyed = event {
+                    let _ = state_clone.shutdown();
+                }
+            });
+
+            app.manage(state);
+            Ok(())
+        })
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![tick, update_entity_property])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
