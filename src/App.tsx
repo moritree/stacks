eeed03c4e1a79f4399ab6abc24be9f2babc9017b
code <leone.zuni@gmail.com>
2025-02-13 13:30:19 +1300

    Port basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,12 @@ import Inspector from "./Inspector";
 export default class App extends Component {
   render() {
     return (
-      <div class="app">
-        <div class="top-app-container">
+      <div class="flex flex-col h-screen z-10">
+        <div class="flex w-full h-[720px]">
           <Scene />
           <Inspector />
         </div>
-        <div class="bottom-space" />
+        <div class="bg-(--secondary-color) border-t border-(--border-color) flex-1 h-full" />
       </div>
     );
   }
