7250a6211d8d8dbcc500e05aa1ccc1beba5b374b
code <leone.zuni@gmail.com>
2025-02-14 14:02:22 +1300

    Remove unused

--- src/Inspector.tsx
+++ src/Inspector.tsx
@@ -3,7 +3,7 @@ import { Component } from "preact";
 export default class Inspector extends Component {
   render() {
     return (
-      <div class="w-40 h-full bg-(--secondary-color) border-l border-(--border-color) flex-1">
+      <div class="h-full bg-(--secondary-color) border-l border-(--border-color) flex-1">
         <h1>Inspector</h1>
       </div>
     );
