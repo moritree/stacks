7a315c595616b894e9c555427976bfc7b6e812dc
code <leone.zuni@gmail.com>
2025-02-24 09:40:10 +1300

    Remove comments

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -12,18 +12,12 @@ pub fn run() {
         .setup(|app| {
             let window = app.get_webview_window("main").unwrap();
             let state = init_lua_thread(window.clone());
-
-            // Shutting down the clone WILL shut down the actual original state.
-            // The `mpsc::Sender` type specifically is designed to work this way
-            // a clone isn't a deep copy of the channel, but another handle to it
-            // We need a clone bc rust ownership means scope changes lose the var
             let state_clone = state.clone();
             window.on_window_event(move |event| {
                 if let tauri::WindowEvent::Destroyed = event {
                     let _ = state_clone.shutdown();
                 }
             });
-
             app.manage(state);
             Ok(())
         })
