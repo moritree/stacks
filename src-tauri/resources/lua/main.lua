40230f30788a81700da168fed42a9479de2bb516
code <leone.zuni@gmail.com>
2025-03-20 16:14:52 +1300

    Remove position reference entities

--- src-tauri/resources/lua/main.lua
+++ src-tauri/resources/lua/main.lua
@@ -30,24 +30,6 @@ end]]
                 size = { width = 200, height = 100 },
                 color = "#ff0000"
             }),
-            top_left = Entity:new({
-                type = "rect",
-                pos = { x = 0, y = 0 },
-                size = { width = 10, height = 10 },
-                color = "#00ff00"
-            }),
-            center = Entity:new({
-                type = "rect",
-                pos = { x = 635, y = 355 },
-                size = { width = 10, height = 10 },
-                color = "#00ffff"
-            }),
-            bottom_right = Entity:new({
-                type = "rect",
-                pos = { x = 1270, y = 710 },
-                size = { width = 10, height = 10 },
-                color = "#0000ff"
-            })
         }
     })
 }
