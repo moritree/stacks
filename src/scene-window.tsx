6b2b0eb30871df54c2238a8816146b0718c96d39
code <leone.zuni@gmail.com>
2025-03-31 15:41:32 +1300

    Listen for resize event on scene window, don't need to check if it's coming from the right window

--- src/scene-window.tsx
+++ src/scene-window.tsx
@@ -7,6 +7,7 @@ import { save, open, message } from "@tauri-apps/plugin-dialog";
 import { WebviewWindow } from "@tauri-apps/api/webviewWindow";
 import { useEffect, useState } from "preact/hooks";
 import { Entity } from "./entity/entity-type";
+import { getCurrentWindow } from "@tauri-apps/api/window";
 
 const SCENE_BASE_SIZE = {
   width: 1280,
@@ -95,41 +96,31 @@ export default function Scene() {
     }
 
     async function setupResizeListener() {
-      const unsubscribe = await listen<{ width: number; height: number }>(
-        "tauri://resize",
-        async (e) => {
-          // Do nothing if the window being resized is a different one
-          // Yes this is janky and I should write a better solution
-          const thisWindowSize = await WebviewWindow.getCurrent().size();
-          if (
-            thisWindowSize.width != e.payload.width ||
-            thisWindowSize.height != e.payload.height
-          ) {
-            return;
-          }
+      const unsubscribe = await getCurrentWindow().listen<{
+        width: number;
+        height: number;
+      }>("tauri://resize", async (e) => {
+        setSelectedId(undefined);
 
-          setSelectedId(undefined);
+        const scaleFactor: number = await invoke("window_scale");
+        const contentHeight = document.documentElement.clientHeight; // content area dimensions (excluding title bar)
+        const windowHeight = e.payload.height; // gives us the full window dimensions
+        const titleBarHeight = windowHeight / scaleFactor - contentHeight; // Calculate title bar height dynamically
 
-          const scaleFactor: number = await invoke("window_scale");
-          const contentHeight = document.documentElement.clientHeight; // content area dimensions (excluding title bar)
-          const windowHeight = e.payload.height; // gives us the full window dimensions
-          const titleBarHeight = windowHeight / scaleFactor - contentHeight; // Calculate title bar height dynamically
+        const newScale = e.payload.width / SCENE_BASE_SIZE.width;
+        setTransformScale(scaleFactor / newScale);
 
-          const newScale = e.payload.width / SCENE_BASE_SIZE.width;
-          setTransformScale(scaleFactor / newScale);
-
-          invoke("resize_window", {
-            width: Math.round(SCENE_BASE_SIZE.width * newScale),
-            height: Math.round(
-              SCENE_BASE_SIZE.height * newScale + titleBarHeight * scaleFactor,
-            ),
-          });
-          document.documentElement.style.setProperty(
-            `--scene-scale`,
-            newScale / scaleFactor + "",
-          );
-        },
-      );
+        invoke("resize_window", {
+          width: Math.round(SCENE_BASE_SIZE.width * newScale),
+          height: Math.round(
+            SCENE_BASE_SIZE.height * newScale + titleBarHeight * scaleFactor,
+          ),
+        });
+        document.documentElement.style.setProperty(
+          `--scene-scale`,
+          newScale / scaleFactor + "",
+        );
+      });
       listeners.push(unsubscribe);
 
       emit("tauri://resize", await WebviewWindow.getCurrent().size()).then(() =>
